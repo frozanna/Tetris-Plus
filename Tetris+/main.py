0ce7c7607e23a503284529b3f5a1f746d578be6f
frozanna <ania.s.731@gmail.com>
2019-03-30 21:56:39 +0100

    poprawiony

--- Tetris+/main.py
+++ Tetris+/main.py
@@ -5,15 +5,27 @@ ROWS = 20
 COLUMNS = 10
 
 SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 660
+SCREEN_HEIGHT = 620
 
-SURF_WIDTH = 300
-SURF_HEIGHT = 600
+SURF_WIDTH = 250
+SURF_HEIGHT = 500
 
 first_elem_x = (SCREEN_WIDTH - SURF_WIDTH)/2
-first_elem_y = (SCREEN_HEIGHT - SURF_HEIGHT)
+first_elem_y = (SCREEN_HEIGHT - SURF_HEIGHT) - 40
 
-elem_size = 30
+elem_size = 25
+
+class Grid(object):
+    def __init__(self):
+        grid = [[0 for x in range(COLUMNS)] for x in range(ROWS)]
+
+
+    def draw_grid(self,screen):
+        empty_block = pygame.image.load("empty.png")
+        for i in range(ROWS):
+            for j in range(COLUMNS):
+                screen.blit(empty_block, (first_elem_x + j * elem_size, first_elem_y + i * elem_size))
+
 
 
 class Game(object):
@@ -23,21 +35,12 @@ class Game(object):
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.delta = 0.0
-
-    def draw_grid(self):
-        empty_block = pygame.image.load("empty.png")
-        for i in range(ROWS):
-            for j in range(COLUMNS):
-                self.screen.blit(empty_block, (first_elem_x + j*elem_size,first_elem_y + i*elem_size))
-                pass
+        self.grid = Grid()
 
     def draw_game(self):
         title_img = pygame.image.load("title.png")
-        self.screen.blit(title_img, (SCREEN_WIDTH / 2 - 75, 10))
-
-        self.draw_grid()
-        grid = [[0 for x in range(COLUMNS)] for x in range(ROWS)]
-
+        self.screen.blit(title_img, (SCREEN_WIDTH / 2 - 75, 20))
+        self.grid.draw_grid(self.screen)
 
     def start_game(self):
         self.draw_game()
@@ -49,15 +52,14 @@ class Game(object):
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     sys.exit(0)
 
-            # self.delta += self.clock.tick() / 1000.0
-            # while self.delta > 1 / self.tps:
-            #     self.tick()
-            #     self.delta -= 1 / self.tps
-
             pygame.display.flip()
 
 
-def start():
-    Game().start_game()
 
-start()
+Game().start_game()
+
+
+# self.delta += self.clock.tick() / 1000.0
+# while self.delta > 1 / self.tps:
+#     self.tick()
+#     self.delta -= 1 / self.tps
